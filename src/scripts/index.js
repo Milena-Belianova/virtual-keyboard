ed1b8cb2f8066e8e6bef938e19731b4ffdb950f8
Milena Belianova <milena.belianova@gmail.com>
2022-05-09 18:39:05 +0400

    feat: push only files from dist folder

--- src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-import '../styles/normalize.css';
-import '../styles/style.scss';
-import { renderMainPage } from './render-main-page';
-
-renderMainPage(document.body);
--- src/scripts/insert-in-textarea.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-export const insertInTextarea = (textarea, text) => {
-  const cursorStart = textarea.selectionStart;
-  textarea.value = textarea.value.slice(0, textarea.selectionStart)
-            + text
-            + textarea.value.slice(textarea.selectionStart);
-  textarea.selectionStart = cursorStart + 1;
-  textarea.selectionEnd = textarea.selectionStart;
-};
